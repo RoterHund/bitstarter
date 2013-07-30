2da753c9daeaa8ded172c91ded7e3d12cbbfe8a7
Alan Foxton <a_foxton@yahoo.co.uk>
2013-07-30 15:41:54 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var mybuffer = readFileSync("index.html");
+  response.send(mybuffer);
 });
 
 var port = process.env.PORT || 5000;
